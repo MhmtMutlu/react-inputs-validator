345f1047d76720f35fffee5d9e9043cc17f8a8cb
movwf <movwf@protonmail.com>
2021-08-01 20:49:51 +0300

    Refactor: Password validation RegExp function dynamized with minLength, maxLength props.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
-import { ExampleComponent } from '.'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { ValidatedInput } from '.'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
+let container;
+
+beforeAll(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+describe('Validated Input', () => {
+  act(()=>{
+    ReactDOM.render(<ValidatedInput type="email" errorMessage="Not a valid e-mail!" />, container);
+  })
+
+  const errorMessage
+
+  it('is email', () => {
     expect(ExampleComponent).toBeTruthy()
   })
 })
